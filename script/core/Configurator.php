972a97f3a2f63de8946c886127ef5e4851327b83
ksm <kasp89s@gmail.com>
2025-10-31 13:39:04 +0200

    Sland #1.053

--- script/core/Configurator.php
+++ script/core/Configurator.php
@@ -3,8 +3,13 @@ namespace Slando\core;
 
 class Configurator
 {
+    private static $_config;
+
     public static function load()
     {
-        return require_once __DIR__ . '/../config.php';
+        if (empty(self::$_config))
+            self::$_config = require_once __DIR__ . '/../config.php';
+
+        return self::$_config;
     }
 }
