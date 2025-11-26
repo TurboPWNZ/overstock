c2456ddbfdf45897c6d386c8bc8ad33847f5249e
ksm <kasp89s@gmail.com>
2025-11-26 16:20:04 +0200

    olx #0.045 PS

--- script/core/payment/privat/Transaction.php
+++ script/core/payment/privat/Transaction.php
@@ -3,10 +3,16 @@ namespace Slando\core\payment\privat;
 
 use Curl\Curl;
 use Slando\core\Configurator;
+use Slando\core\i18n\Translation;
+use Slando\core\Logger;
+use Slando\core\olx\db\Account;
+use Slando\core\olx\db\Subscription;
 use Slando\core\olx\db\Transactions;
+use Slando\core\Telegram;
 
 class Transaction
 {
+
     public static function process()
     {
         $transactions = self::loadTransactionList();
@@ -26,9 +32,99 @@ class Transaction
 
     protected static function processTransaction($transaction)
     {
+        $transactionAmount = (int) $transaction['SUM'];
+
         list($telegramID, $subscriptionID, $amount, $desc) = explode(';', $transaction['OSND']);
 
-        var_dump($telegramID, $subscriptionID, $amount, $desc);
+        $subscription = (new Subscription())->findByPk($subscriptionID);
+
+        if (empty($subscription)) {
+            Logger::log('Error payment: ' . var_export($transaction, true));
+
+            return false;
+        }
+
+        $paymentTime = self::calculatePaymentTime($transactionAmount);
+
+        if ($paymentTime === false) {
+            Logger::log('Error payment: ' . var_export($transaction, true));
+
+            return false;
+        }
+
+        self::applyTimeToSubscription($subscription, $paymentTime, $transaction);
+
+        self::sendAccountNotification($subscription, $transactionAmount);
+
+//        var_dump($telegramID, $subscriptionID, $amount, $desc);
+    }
+
+    protected static function sendAccountNotification($subscription, $transactionAmount)
+    {
+        $config = Configurator::load();
+
+        $subscription = (new Subscription())->findByPk($subscription['id']);
+
+        $account = (new Account())->findByPk($subscription['userId']);
+
+        Telegram::setCredentials($config['params']['secrets']['olx']['bot']);
+
+        Telegram::setChatID($account['telegramUserId']);
+
+        $response['responseMessage'] = Translation::text("<b>Платёж успешно получен! 💳✔️</b>\n\n" .
+            "Спасибо за оплату — <b>:amount</b>, и ваша подписка <b>«:subName»</b> была успешно продлена.\n\n" .
+            "⏳ Новый срок действия подписки:\n" .
+            "<b>:expireDate</b>\n\n" .
+            "Подписка активна, и вы продолжите получать новые объявления без задержек.\n\n" .
+            "Спасибо, что выбираете наш сервис! ❤️", [
+                ':amount' => $transactionAmount,
+                ':subName' => $subscription['name'],
+                ':expireDate' => $subscription['validUntil'],
+        ]);
+
+        var_dump($response);
+//        $response['keyboard'] = [
+//            "inline_keyboard" => [
+//                [
+//                    ["text" => Translation::text("📋 Мої підписки"), "callback_data" => "/list"],
+//                    ["text" => Translation::text("🏠 На початок"), "callback_data" => "/start"],
+//                ]
+//            ]
+//        ];
+        Telegram::sendMessageWithKeyboard($response['responseMessage'], []);
+    }
+
+    protected static function applyTimeToSubscription($subscription, $time, $transaction)
+    {
+        if (strtotime($subscription['validUntil']) > time()) {
+            $newTime = strtotime($subscription['validUntil']) +  (60 * 60 * $time);
+        } else {
+            $newTime = time() + (60 * 60 * $time);
+        }
+        (new Subscription())->update('id = :id', [
+            'id' => $subscription['id'],
+            'nextTime' => date('Y-m-d H:i:s', time() + 60 * 15),
+            'validUntil' => date('Y-m-d H:i:s', $newTime),
+            'isEditInProgress' => 0,
+        ]);
+
+        (new Transactions())->insert([
+            'paymentTransactionId' => $transaction['ID'],
+            'subscriptionId' => $subscription['id'],
+            'amount' => $transaction['SUM'],
+            'date' => date('Y-m-d H:i:s', time())
+        ]);
+    }
+
+    protected static function calculatePaymentTime($amount)
+    {
+        switch($amount) {
+            case 20: return 24;
+            case 50: return 24 * 3;
+            case 100: return 24 * 7;
+            case 200: return 24 * 30;
+            default: return false;
+        }
     }
 
     protected static function isTransactionNotProcessed($transaction)
